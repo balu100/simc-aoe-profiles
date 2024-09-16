c9a8febdf184dfb5314149afad5d9e81986e1132
balu100 <ebalu1@hotmail.com>
2024-09-16 09:54:41 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@
 ./simc TWW1_Raid.simc html=12Target_LightMovement.html fight_style=LightMovement desired_targets=12 target_error=1.0
 ./simc TWW1_Raid.simc html=12Target_HeavyMovement.html fight_style=HeavyMovement desired_targets=12 target_error=1.0
 ./simc TWW1_Raid.simc html=12Target_HelterSkelter.html fight_style=HelterSkelter desired_targets=12 target_error=1.0
-./simc TWW1_Raid.simc html=20Target_LightMovement.html fight_style=LightMovement desired_targets=20 target_error=1.0
-./simc TWW1_Raid.simc html=20Target_HeavyMovement.html fight_style=HeavyMovement desired_targets=20 target_error=1.0
-./simc TWW1_Raid.simc html=20Target_HelterSkelter.html fight_style=HelterSkelter desired_targets=20 target_error=1.0
+#./simc TWW1_Raid.simc html=20Target_LightMovement.html fight_style=LightMovement desired_targets=20 target_error=1.0
+#./simc TWW1_Raid.simc html=20Target_HeavyMovement.html fight_style=HeavyMovement desired_targets=20 target_error=1.0
+#./simc TWW1_Raid.simc html=20Target_HelterSkelter.html fight_style=HelterSkelter desired_targets=20 target_error=1.0
