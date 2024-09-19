87f128e50c606aba8075f0cf7545e55296ea3e8a
balu100 <ebalu1@hotmail.com>
2024-09-19 13:20:35 +0200

    Update torun.sh

--- torun.sh
+++ torun.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-apk update
-apk add --no-cache nano git
-rm -rf /opt/simc-aoe-profiles
-git -C /opt clone https://github.com/balu100/simc-aoe-profiles.git
-cp -r -f /opt/simc-aoe-profiles/* /app/SimulationCraft/profiles/
-./simc TWW1_Raid_NW_10.simc html=/opt/outside/TWW1_Raid_NW_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_NW_10.simc html=/opt/outside/TWW1_Raid_NW_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_NW_15.simc html=/opt/outside/TWW1_Raid_NW_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_NW_15.simc html=/opt/outside/TWW1_Raid_NW_15_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_NW_20.simc html=/opt/outside/TWW1_Raid_NW_20.html target_error=1.0 ptr=0
-./simc TWW1_Raid_NW_20.simc html=/opt/outside/TWW1_Raid_NW_20_PTR.html target_error=1.0 ptr=1
+#apk update
+#apk add --no-cache nano git
+#rm -rf /opt/simc-aoe-profiles
+#git -C /opt clone https://github.com/balu100/simc-aoe-profiles.git
+#cp -r -f /opt/simc-aoe-profiles/* /app/SimulationCraft/profiles/
+#./simc TWW1_Raid_NW_10.simc html=/opt/outside/TWW1_Raid_NW_10.html target_error=1.0 ptr=0
+#./simc TWW1_Raid_NW_10.simc html=/opt/outside/TWW1_Raid_NW_10_PTR.html target_error=1.0 ptr=1
+#./simc TWW1_Raid_NW_15.simc html=/opt/outside/TWW1_Raid_NW_15.html target_error=1.0 ptr=0
+#./simc TWW1_Raid_NW_15.simc html=/opt/outside/TWW1_Raid_NW_15_PTR.html target_error=1.0 ptr=1
+#./simc TWW1_Raid_NW_20.simc html=/opt/outside/TWW1_Raid_NW_20.html target_error=1.0 ptr=0
+#./simc TWW1_Raid_NW_20.simc html=/opt/outside/TWW1_Raid_NW_20_PTR.html target_error=1.0 ptr=1
+
+# Run an infinite loop with no task
+while true; do
+  # Just sleep to keep the script running
+  sleep 86400 # Sleep for 24 hours
+done
