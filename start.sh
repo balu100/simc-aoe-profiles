fd269eec11222e1d954ce1687d202a789299a868
balu100 <ebalu1@hotmail.com>
2024-09-19 12:01:17 +0200

    a

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 
 # Remove the existing directory if it exists
 rm -rf /opt/simc-aoe-profiles
@@ -9,8 +9,11 @@ git -C /opt clone https://github.com/balu100/simc-aoe-profiles.git
 # Copy files to the destination directory
 cp -r -f /opt/simc-aoe-profiles/* /app/SimulationCraft/profiles/
 
+# Change directory to /app/SimulationCraft
+cd /app/SimulationCraft || exit 1  # Exit if the directory doesn't exist
+
 # Read commands from the /app/SimulationCraft/profiles/torun file and execute them
 while IFS= read -r line; do
-    # Execute the command from the file
-    $line
+    # Execute the command using ash
+    /bin/ash -c "$line"
 done < /app/SimulationCraft/profiles/torun
