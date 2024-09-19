720f82c2615ff10a20f6f0c1d63b4014b7753d7b
balu100 <ebalu1@hotmail.com>
2024-09-19 09:57:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,3 +12,17 @@
 #./simc TWW1_Raid.simc html=/opt/outside/20Target_LightMovement.html fight_style=LightMovement desired_targets=20 target_error=1.0
 #./simc TWW1_Raid.simc html=/opt/outside/20Target_HeavyMovement.html fight_style=HeavyMovement desired_targets=20 target_error=1.0
 #./simc TWW1_Raid.simc html=/opt/outside/20Target_HelterSkelter.html fight_style=HelterSkelter desired_targets=20 target_error=1.0
+./simc TWW1_Raid.simc html=/opt/outside/6Target_LightMovement_PTR.html fight_style=LightMovement desired_targets=6 target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/6Target_HelterSkelter_PTR.html fight_style=HelterSkelter desired_targets=6 target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/6Target_HeavyMovement_PTR.html fight_style=HeavyMovement desired_targets=6 target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/8Target_LightMovement_PTR.html fight_style=LightMovement desired_targets=8 target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/8Target_HeavyMovement_PTR.html fight_style=HeavyMovement desired_targets=8 target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/8Target_HelterSkelter_PTR.html fight_style=HelterSkelter desired_targets=8 target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/DungeonSlice_PTR.html fight_style=DungeonSlice target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/Patchwerk_PTR.html fight_style=Patchwerk target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/12Target_LightMovement_PTR.html fight_style=LightMovement desired_targets=12 target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/12Target_HeavyMovement_PTR.html fight_style=HeavyMovement desired_targets=12 target_error=1.0 ptr=1
+./simc TWW1_Raid.simc html=/opt/outside/12Target_HelterSkelter_PTR.html fight_style=HelterSkelter desired_targets=12 target_error=1.0 ptr=1
+#./simc TWW1_Raid.simc html=/opt/outside/20Target_LightMovement_PTR.html fight_style=LightMovement desired_targets=20 target_error=1.0 ptr=1
+#./simc TWW1_Raid.simc html=/opt/outside/20Target_HeavyMovement_PTR.html fight_style=HeavyMovement desired_targets=20 target_error=1.0 ptr=1
+#./simc TWW1_Raid.simc html=/opt/outside/20Target_HelterSkelter_PTR.html fight_style=HelterSkelter desired_targets=20 target_error=1.0 ptr=1
