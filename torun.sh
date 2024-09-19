0313029f9966f7c08617b521f7ac4ff98f2e4878
balu100 <ebalu1@hotmail.com>
2024-09-19 23:53:25 +0200

    Update torun.sh

--- torun.sh
+++ torun.sh
@@ -6,7 +6,7 @@ rm -rf /opt/simc-aoe-profiles
 git -C /opt clone https://github.com/balu100/simc-aoe-profiles.git
 cp -r -f /opt/simc-aoe-profiles/* /app/SimulationCraft/profiles/
 
-#siege-of-boralus
+#siege-of-boralus 
 ./simc TWW1_Raid_SoB_10.simc html=/opt/outside/TWW1_Raid_SoB_10.html target_error=1.0 ptr=0
 ./simc TWW1_Raid_SoB_10.simc html=/opt/outside/TWW1_Raid_SoB_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_SoB_15.simc html=/opt/outside/TWW1_Raid_SoB_15.html target_error=1.0 ptr=0
