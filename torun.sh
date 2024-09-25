7f04b8d2fce1fbf7d699775528a3e6639eac6a35
balu100 <ebalu1@hotmail.com>
2024-09-25 11:00:40 +0200

    xml

--- torun.sh
+++ torun.sh
@@ -7,61 +7,61 @@ git -C /opt clone https://github.com/balu100/simc-aoe-profiles.git
 cp -r -f /opt/simc-aoe-profiles/* /app/SimulationCraft/profiles/
 
 echo "siege-of-boralu"
-./simc TWW1_Raid_SoB_10.simc html=/opt/outside/TWW1_Raid_SoB_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_SoB_10.simc html=/opt/outside/TWW1_Raid_SoB_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_SoB_15.simc html=/opt/outside/TWW1_Raid_SoB_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_SoB_15.simc html=/opt/outside/TWW1_Raid_SoB_15_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_SoB_10.simc xml=/opt/outside/TWW1_Raid_SoB_10.xml html=/opt/outside/TWW1_Raid_SoB_10.html target_error=1.0 ptr=0
+./simc TWW1_Raid_SoB_10.simc xml=/opt/outside/TWW1_Raid_SoB_10_PTR.xml html=/opt/outside/TWW1_Raid_SoB_10_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_SoB_15.simc xml=/opt/outside/TWW1_Raid_SoB_15.xml html=/opt/outside/TWW1_Raid_SoB_15.html target_error=1.0 ptr=0
+./simc TWW1_Raid_SoB_15.simc xml=/opt/outside/TWW1_Raid_SoB_15_PTR.xml html=/opt/outside/TWW1_Raid_SoB_15_PTR.html target_error=1.0 ptr=1
 
 echo "mists-of-tirna-scithe"
-./simc TWW1_Raid_MoTS_10.simc html=/opt/outside/TWW1_Raid_MoTS_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_MoTS_10.simc html=/opt/outside/TWW1_Raid_MoTS_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_MoTS_15.simc html=/opt/outside/TWW1_Raid_MoTS_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_MoTS_15.simc html=/opt/outside/TWW1_Raid_MoTS_15_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_MoTS_10.simc xml=/opt/outside/TWW1_Raid_MoTS_10.xml html=/opt/outside/TWW1_Raid_MoTS_10.html target_error=1.0 ptr=0
+./simc TWW1_Raid_MoTS_10.simc xml=/opt/outside/TWW1_Raid_MoTS_10_PTR.xml html=/opt/outside/TWW1_Raid_MoTS_10_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_MoTS_15.simc xml=/opt/outside/TWW1_Raid_MoTS_15.xml html=/opt/outside/TWW1_Raid_MoTS_15.html target_error=1.0 ptr=0
+./simc TWW1_Raid_MoTS_15.simc xml=/opt/outside/TWW1_Raid_MoTS_15_PTR.xml html=/opt/outside/TWW1_Raid_MoTS_15_PTR.html target_error=1.0 ptr=1
 
 
 echo "the-necrotic-wake"
-./simc TWW1_Raid_TNW_10.simc html=/opt/outside/TWW1_Raid_TNW_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_TNW_10.simc html=/opt/outside/TWW1_Raid_TNW_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_TNW_15.simc html=/opt/outside/TWW1_Raid_TNW_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_TNW_15.simc html=/opt/outside/TWW1_Raid_TNW_15_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_TNW_10.simc xml=/opt/outside/TWW1_Raid_TNW_10.xml html=/opt/outside/TWW1_Raid_TNW_10.html target_error=1.0 ptr=0
+./simc TWW1_Raid_TNW_10.simc xml=/opt/outside/TWW1_Raid_TNW_10_PTR.xml html=/opt/outside/TWW1_Raid_TNW_10_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_TNW_15.simc xml=/opt/outside/TWW1_Raid_TNW_15.xml html=/opt/outside/TWW1_Raid_TNW_15.html target_error=1.0 ptr=0
+./simc TWW1_Raid_TNW_15.simc xml=/opt/outside/TWW1_Raid_TNW_15_PTR.xml html=/opt/outside/TWW1_Raid_TNW_15_PTR.html target_error=1.0 ptr=1
 
 
 echo "ara-kara-city-of-echoes"
-./simc TWW1_Raid_AKCOE_10.simc html=/opt/outside/TWW1_Raid_AKCOE_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_AKCOE_10.simc html=/opt/outside/TWW1_Raid_AKCOE_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_AKCOE_15.simc html=/opt/outside/TWW1_Raid_AKCOE_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_AKCOE_15.simc html=/opt/outside/TWW1_Raid_AKCOE_15_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_AKCOE_10.simc xml=/opt/outside/TWW1_Raid_AKCOE_10.xml html=/opt/outside/TWW1_Raid_AKCOE_10.html target_error=1.0 ptr=0
+./simc TWW1_Raid_AKCOE_10.simc xml=/opt/outside/TWW1_Raid_AKCOE_10_PTR.xml html=/opt/outside/TWW1_Raid_AKCOE_10_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_AKCOE_15.simc xml=/opt/outside/TWW1_Raid_AKCOE_15.xml html=/opt/outside/TWW1_Raid_AKCOE_15.html target_error=1.0 ptr=0
+./simc TWW1_Raid_AKCOE_15.simc xml=/opt/outside/TWW1_Raid_AKCOE_15_PTR.xml html=/opt/outside/TWW1_Raid_AKCOE_15_PTR.html target_error=1.0 ptr=1
 
 
 echo "city-of-threads"
-./simc TWW1_Raid_CoT_10.simc html=/opt/outside/TWW1_Raid_CoT_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_CoT_10.simc html=/opt/outside/TWW1_Raid_CoT_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_CoT_15.simc html=/opt/outside/TWW1_Raid_CoT_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_CoT_15.simc html=/opt/outside/TWW1_Raid_CoT_15_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_CoT_10.simc xml=/opt/outside/TWW1_Raid_CoT_10.xml html=/opt/outside/TWW1_Raid_CoT_10.html target_error=1.0 ptr=0
+./simc TWW1_Raid_CoT_10.simc xml=/opt/outside/TWW1_Raid_CoT_10_PTR.xml html=/opt/outside/TWW1_Raid_CoT_10_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_CoT_15.simc xml=/opt/outside/TWW1_Raid_CoT_15.xml html=/opt/outside/TWW1_Raid_CoT_15.html target_error=1.0 ptr=0
+./simc TWW1_Raid_CoT_15.simc xml=/opt/outside/TWW1_Raid_CoT_15_PTR.xml html=/opt/outside/TWW1_Raid_CoT_15_PTR.html target_error=1.0 ptr=1
 
 
 echo "the-dawnbreaker"
-./simc TWW1_Raid_TD_10.simc html=/opt/outside/TWW1_Raid_TD_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_TD_10.simc html=/opt/outside/TWW1_Raid_TD_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_TD_15.simc html=/opt/outside/TWW1_Raid_TD_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_TD_15.simc html=/opt/outside/TWW1_Raid_TD_15_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_TD_10.simc xml=/opt/outside/TWW1_Raid_TD_10.xml html=/opt/outside/TWW1_Raid_TD_10.html target_error=1.0 ptr=0
+./simc TWW1_Raid_TD_10.simc xml=/opt/outside/TWW1_Raid_TD_10_PTR.xml html=/opt/outside/TWW1_Raid_TD_10_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_TD_15.simc xml=/opt/outside/TWW1_Raid_TD_15.xml html=/opt/outside/TWW1_Raid_TD_15.html target_error=1.0 ptr=0
+./simc TWW1_Raid_TD_15.simc xml=/opt/outside/TWW1_Raid_TD_15_PTR.xml html=/opt/outside/TWW1_Raid_TD_15_PTR.html target_error=1.0 ptr=1
 
 
 echo "the-stonevault"
-./simc TWW1_Raid_TS_10.simc html=/opt/outside/TWW1_Raid_TS_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_TS_10.simc html=/opt/outside/TWW1_Raid_TS_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_TS_15.simc html=/opt/outside/TWW1_Raid_TS_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_TS_15.simc html=/opt/outside/TWW1_Raid_TS_15_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_TS_10.simc xml=/opt/outside/TWW1_Raid_TS_10.xml html=/opt/outside/TWW1_Raid_TS_10.html target_error=1.0 ptr=0
+./simc TWW1_Raid_TS_10.simc xml=/opt/outside/TWW1_Raid_TS_10_PTR.xml html=/opt/outside/TWW1_Raid_TS_10_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_TS_15.simc xml=/opt/outside/TWW1_Raid_TS_15.xml html=/opt/outside/TWW1_Raid_TS_15.html target_error=1.0 ptr=0
+./simc TWW1_Raid_TS_15.simc xml=/opt/outside/TWW1_Raid_TS_15_PTR.xml html=/opt/outside/TWW1_Raid_TS_15_PTR.html target_error=1.0 ptr=1
 
 echo "grim-batol"
-./simc TWW1_Raid_GB_10.simc html=/opt/outside/TWW1_Raid_GB_10.html target_error=1.0 ptr=0
-./simc TWW1_Raid_GB_10.simc html=/opt/outside/TWW1_Raid_GB_10_PTR.html target_error=1.0 ptr=1
-./simc TWW1_Raid_GB_15.simc html=/opt/outside/TWW1_Raid_GB_15.html target_error=1.0 ptr=0
-./simc TWW1_Raid_GB_15.simc html=/opt/outside/TWW1_Raid_GB_15_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_GB_10.simc xml=/opt/outside/TWW1_Raid_GB_10.xml html=/opt/outside/TWW1_Raid_GB_10.html target_error=1.0 ptr=0
+./simc TWW1_Raid_GB_10.simc xml=/opt/outside/TWW1_Raid_GB_10_PTR.xml html=/opt/outside/TWW1_Raid_GB_10_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid_GB_15.simc xml=/opt/outside/TWW1_Raid_GB_15.xml html=/opt/outside/TWW1_Raid_GB_15.html target_error=1.0 ptr=0
+./simc TWW1_Raid_GB_15.simc xml=/opt/outside/TWW1_Raid_GB_15_PTR.xml html=/opt/outside/TWW1_Raid_GB_15_PTR.html target_error=1.0 ptr=1
 
 echo "single target with aoe"
-./simc TWW1_Raid.simc html=/opt/outside/TTWW1_Raid.html target_error=1.0 ptr=0
-./simc TWW1_Raid.simc html=/opt/outside/TWW1_Raid_PTR.html target_error=1.0 ptr=1
+./simc TWW1_Raid.simc xml=/opt/outside/TTWW1_Raid.xml html=/opt/outside/TTWW1_Raid.html target_error=1.0 ptr=0
+./simc TWW1_Raid.simc xml=/opt/outside/TTWW1_Raid_PTR.xml html=/opt/outside/TWW1_Raid_PTR.html target_error=1.0 ptr=1
 
 
 echo "done"
