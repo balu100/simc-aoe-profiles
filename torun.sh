17235937d2bdc0258589fde81d4c1306cdd40c6e
balu100 <ebalu1@hotmail.com>
2024-10-10 11:26:37 +0200

    Update torun.sh

--- torun.sh
+++ torun.sh
@@ -6,7 +6,6 @@ rm -rf /opt/simc-aoe-profiles
 git -C /opt clone https://github.com/balu100/simc-aoe-profiles.git
 cp -r -f /opt/simc-aoe-profiles/* /app/SimulationCraft/profiles/
 
-
 # Adding blank lines
 echo ""
 echo ""
@@ -28,7 +27,7 @@ shuffled_colors=$(shuffle_colors)
 i=1
 
 # Displaying the message in shuffled rainbow colors with a larger font
-figlet -f big "Version 1.1" | while IFS= read -r line; do
+figlet -f big "Version 1.2" | while IFS= read -r line; do
   # Get the current color from the shuffled list
   color=$(echo $shuffled_colors | cut -d' ' -f$i)
   
@@ -50,80 +49,136 @@ echo ""
 echo ""
 echo ""
 
-echo "best-run-from-rio"
+echo "RIO" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc Bestrunfromraiderio.simc json2=/opt/outside/Bestrunfromraiderio.json html=/opt/outside/Bestrunfromraiderio.html target_error=1.0 ptr=0
 
-
-echo "siege-of-boralu"
+echo ""
+echo ""
+echo ""
+echo "SOB" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid_SoB_10.simc json2=/opt/outside/TWW1_Raid_SoB_10.json html=/opt/outside/TWW1_Raid_SoB_10.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_SoB_10.simc json2=/opt/outside/TWW1_Raid_SoB_10_PTR.json html=/opt/outside/TWW1_Raid_SoB_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_SoB_15.simc json2=/opt/outside/TWW1_Raid_SoB_15.json html=/opt/outside/TWW1_Raid_SoB_15.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_SoB_15.simc json2=/opt/outside/TWW1_Raid_SoB_15_PTR.json html=/opt/outside/TWW1_Raid_SoB_15_PTR.html target_error=1.0 ptr=1
 
-echo "mists-of-tirna-scithe"
+echo ""
+echo ""
+echo ""
+echo "MITS" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid_MoTS_10.simc json2=/opt/outside/TWW1_Raid_MoTS_10.json html=/opt/outside/TWW1_Raid_MoTS_10.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_MoTS_10.simc json2=/opt/outside/TWW1_Raid_MoTS_10_PTR.json html=/opt/outside/TWW1_Raid_MoTS_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_MoTS_15.simc json2=/opt/outside/TWW1_Raid_MoTS_15.json html=/opt/outside/TWW1_Raid_MoTS_15.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_MoTS_15.simc json2=/opt/outside/TWW1_Raid_MoTS_15_PTR.json html=/opt/outside/TWW1_Raid_MoTS_15_PTR.html target_error=1.0 ptr=1
 
 
-echo "the-necrotic-wake"
+echo ""
+echo ""
+echo ""
+echo "NW" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid_TNW_10.simc json2=/opt/outside/TWW1_Raid_TNW_10.json html=/opt/outside/TWW1_Raid_TNW_10.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_TNW_10.simc json2=/opt/outside/TWW1_Raid_TNW_10_PTR.json html=/opt/outside/TWW1_Raid_TNW_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_TNW_15.simc json2=/opt/outside/TWW1_Raid_TNW_15.json html=/opt/outside/TWW1_Raid_TNW_15.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_TNW_15.simc json2=/opt/outside/TWW1_Raid_TNW_15_PTR.json html=/opt/outside/TWW1_Raid_TNW_15_PTR.html target_error=1.0 ptr=1
 
 
-echo "ara-kara-city-of-echoes"
+echo ""
+echo ""
+echo ""
+echo "COE" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid_AKCOE_10.simc json2=/opt/outside/TWW1_Raid_AKCOE_10.json html=/opt/outside/TWW1_Raid_AKCOE_10.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_AKCOE_10.simc json2=/opt/outside/TWW1_Raid_AKCOE_10_PTR.json html=/opt/outside/TWW1_Raid_AKCOE_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_AKCOE_15.simc json2=/opt/outside/TWW1_Raid_AKCOE_15.json html=/opt/outside/TWW1_Raid_AKCOE_15.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_AKCOE_15.simc json2=/opt/outside/TWW1_Raid_AKCOE_15_PTR.json html=/opt/outside/TWW1_Raid_AKCOE_15_PTR.html target_error=1.0 ptr=1
 
 
-echo "city-of-threads"
+echo ""
+echo ""
+echo ""
+echo "COT" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid_CoT_10.simc json2=/opt/outside/TWW1_Raid_CoT_10.json html=/opt/outside/TWW1_Raid_CoT_10.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_CoT_10.simc json2=/opt/outside/TWW1_Raid_CoT_10_PTR.json html=/opt/outside/TWW1_Raid_CoT_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_CoT_15.simc json2=/opt/outside/TWW1_Raid_CoT_15.json html=/opt/outside/TWW1_Raid_CoT_15.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_CoT_15.simc json2=/opt/outside/TWW1_Raid_CoT_15_PTR.json html=/opt/outside/TWW1_Raid_CoT_15_PTR.html target_error=1.0 ptr=1
 
 
-echo "the-dawnbreaker"
+echo ""
+echo ""
+echo ""
+echo "TD" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid_TD_10.simc json2=/opt/outside/TWW1_Raid_TD_10.json html=/opt/outside/TWW1_Raid_TD_10.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_TD_10.simc json2=/opt/outside/TWW1_Raid_TD_10_PTR.json html=/opt/outside/TWW1_Raid_TD_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_TD_15.simc json2=/opt/outside/TWW1_Raid_TD_15.json html=/opt/outside/TWW1_Raid_TD_15.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_TD_15.simc json2=/opt/outside/TWW1_Raid_TD_15_PTR.json html=/opt/outside/TWW1_Raid_TD_15_PTR.html target_error=1.0 ptr=1
 
 
-echo "the-stonevault"
+echo ""
+echo ""
+echo ""
+echo "TS" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid_TS_10.simc json2=/opt/outside/TWW1_Raid_TS_10.json html=/opt/outside/TWW1_Raid_TS_10.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_TS_10.simc json2=/opt/outside/TWW1_Raid_TS_10_PTR.json html=/opt/outside/TWW1_Raid_TS_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_TS_15.simc json2=/opt/outside/TWW1_Raid_TS_15.json html=/opt/outside/TWW1_Raid_TS_15.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_TS_15.simc json2=/opt/outside/TWW1_Raid_TS_15_PTR.json html=/opt/outside/TWW1_Raid_TS_15_PTR.html target_error=1.0 ptr=1
 
-echo "grim-batol"
+echo ""
+echo ""
+echo ""
+echo "GB" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid_GB_10.simc json2=/opt/outside/TWW1_Raid_GB_10.json html=/opt/outside/TWW1_Raid_GB_10.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_GB_10.simc json2=/opt/outside/TWW1_Raid_GB_10_PTR.json html=/opt/outside/TWW1_Raid_GB_10_PTR.html target_error=1.0 ptr=1
 ./simc TWW1_Raid_GB_15.simc json2=/opt/outside/TWW1_Raid_GB_15.json html=/opt/outside/TWW1_Raid_GB_15.html target_error=1.0 ptr=0
 #./simc TWW1_Raid_GB_15.simc json2=/opt/outside/TWW1_Raid_GB_15_PTR.json html=/opt/outside/TWW1_Raid_GB_15_PTR.html target_error=1.0 ptr=1
 
-echo "single target with aoe"
+echo ""
+echo ""
+echo ""
+echo "ST" | figlet -f big
+echo ""
+echo ""
+echo ""
 ./simc TWW1_Raid.simc json2=/opt/outside/TWW1_Raid.json html=/opt/outside/TWW1_Raid.html target_error=1.0 ptr=0
 #./simc TWW1_Raid.simc json2=/opt/outside/TWW1_Raid_PTR.json html=/opt/outside/TWW1_Raid_PTR.html target_error=1.0 ptr=1
 
-
-echo "done"
-echo "done 10%"
-echo "done 20%"
-echo "done 30%"
-echo "done 40%"
-echo "done 50%"
-echo "done 60%"
-echo "done 70%"
-echo "done 80%"
-echo "done 90%"
-echo "done 100%"
-
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "DONE" | figlet -f big
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 
 # Run an infinite loop with no task
 while true; do
