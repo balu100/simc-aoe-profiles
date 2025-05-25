b8351b46262fa4e465d370a252abab2751f9d7b4
balu100 <ebalu1@hotmail.com>
2025-05-26 00:22:06 +0200

    Update torun.sh

--- torun.sh
+++ torun.sh
@@ -112,7 +112,7 @@ run_simc_with_retry() {
 
 # Initial setup and display version info
 setup_repo
-display_colored_message "Version 1.1.1" "big"
+display_colored_message "Version 2.0.0" "big"
 
 # Simulation scenarios
 scenarios=(
